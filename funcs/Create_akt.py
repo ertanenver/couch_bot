6329308395bb25f7e806a7d2f29ed18cc938a8e7
SHINIAEV_VO <shiniaev_vo@omk.ru>
2024-01-22 00:09:39 +0300

    Что-то добавил, когда-нибудь будем по-человечески обзывать коммиты

--- funcs/Create_akt.py
+++ funcs/Create_akt.py
@@ -1,19 +1,20 @@
-from openpyxl import load_workbook
 from openpyxl.styles import PatternFill
 from openpyxl import Workbook
-import re
 from datetime import datetime
-#couch type: "fio":["kindergarden 1","schoool 35"]
+# couch type: {"fio":["kindergarden 1","schoool 35"]}
 def create_akt(couch:dict):
+    # Читаем словарь, на его основании обзываем листы, ключ идет в название файла как ФИО
     for key, value in couch.items():
         arr = value
         fio = key
     wb = Workbook()
     ws = [wb.create_sheet(title=x) for x in arr]
+    # Удаляем пустой лист, создающийся по умолчанию
     del wb['Sheet']
+    # Получаем сегодняшнюю дату и приводим ее к формату DD.MM.YYYY
     today = f'{datetime.now().day}.{datetime.now().month}.{datetime.now().year}'
 
-    
+    # Сохраняем файл
     wb.save(f'Акт сверки {fio} на {today}.xlsx')
 
 create_akt({'Шиняев Владимир Олегович':["Дс 70", "Медвежонок","Золотой ключик"]})
