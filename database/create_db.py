6362791c154ff08527453ecbc3ae81fa7e0cdbb7
SHINIAEV_VO <shiniaev_vo@omk.ru>
2024-01-22 03:21:17 +0300

    Добавил sqlite

---
+++ database/create_db.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+# Создаем подключение к базе данных (файл my_database.db будет создан)
+connection = sqlite3.connect('couch_bot.db')
+
+cursor = connection.cursor()
+
+# Создаем таблицу Users
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users (
+id INTEGER PRIMARY KEY,
+fio TEXT NOT NULL PRIMARY KEY,
+permission TEXT NOT NULL,
+work_place TEXT NOT NULL,
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
+''')
+
+connection.close()
